c2997e12c804256e6eb87d1663013ea0bd09988c
裴栋 <1109202426@qq.com>
2018-11-27 08:24:52 +0800

    js

--- oiuj.js
+++ oiuj.js
@@ -12,9 +12,32 @@ function flat(arr){
 // console.log(flat(arr));
 let r = arr.join().split(',');
 // console.log(r);
-function flatten(arr) {  
-    return arr.reduce((prev, next)=> {
-        return prev.concat(Array.isArray(next) ? flatten(next) : next);
-    }, []);
+function faltten(arr){
+    return arr.reduce((prev,next)=>{
+        return prev.concat(Array.isArray(next)?faltten(next):next);
+    },[])
 }
-console.log(flatten(arr));
+console.log(faltten(arr));
+
+// 分类 type 值相同的放在一起
+let obj = [{type:1,name:'张三'},{type:2,name:'王五'},{type:1,name:'赵四'},{type:2,name:'王哈'},{type:2,name:'王哈'}];
+let newArr = {};
+obj.forEach(v=>{
+    newArr[v.type] || (newArr[v.type]=[]);
+    console.log(newArr[v.type]);
+    newArr[v.type].push(v);
+})
+console.log(newArr);
+
+function faltten(){
+    //不管有没有异常 finally块中的代码都会执行 当try catch中有return时finally仍然会执行；
+    try{
+        return 1;
+    }catch{
+        return 2;
+    }finally{
+        return 3;
+    }
+}
+console.log(faltten());
+
